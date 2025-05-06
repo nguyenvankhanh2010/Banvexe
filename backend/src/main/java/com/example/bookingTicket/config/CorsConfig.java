b0db9c22ef3d73905f63be89be97c4a1b73f59b8
nguyenvankhanh2010 <vkhanh1224@gmail.com>
2025-05-06 22:23:58 +0700

    add

--- backend/src/main/java/com/example/bookingTicket/config/CorsConfig.java
+++ backend/src/main/java/com/example/bookingTicket/config/CorsConfig.java
@@ -16,25 +16,29 @@ public class CorsConfig {
         System.out.println("Initializing CorsFilter...");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        
+
         // Allow credentials
         config.setAllowCredentials(true);
-        
+
+        // Thêm các origin cho môi trường cục bộ và sản phẩm
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("http://localhost:3001");
         config.addAllowedOrigin("http://localhost:3002");
-        
+        config.addAllowedOrigin("https://banvexe.vercel.app");
+
+        // Thêm các header được phép
         config.addAllowedHeader("Origin");
         config.addAllowedHeader("Content-Type");
         config.addAllowedHeader("Accept");
         config.addAllowedHeader("Authorization");
-        
+
+        // Thêm các phương thức được phép
         config.addAllowedMethod("GET");
         config.addAllowedMethod("POST");
         config.addAllowedMethod("PUT");
         config.addAllowedMethod("DELETE");
         config.addAllowedMethod("OPTIONS");
-        
+
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
